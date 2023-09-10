be0b352f6c24f55f35cd9972f5cd7f9268fe3fea
plengauer <p.lengauer@gmail.com>
2023-09-10 18:41:50 +0000

    deep package structure to avoid collisions

--- package/usr/bin/gitaptly_update.sh
+++ package/usr/bin/gitaptly_update.sh
@@ -3,6 +3,9 @@ set -e
 
 repo_location=/var/lib/gitaptly/apt-repo
 
+# the following line is just for compatibility in case old versions have been installed
+rm $repo_location/pool/main/*.deb
+
 while read line; do
   line=$(echo "$line" | xargs)
   if [ -z "$line" ]; then
@@ -10,7 +13,8 @@ while read line; do
   fi
   owner=$(echo $line | cut -d "/" -f 1)
   repo=$(echo $line | cut -d "/" -f 2)
-  bash /usr/bin/gitaptly_scan_and_download.sh $repo_location/pool/main $owner $repo
+  mkdir -p $repo_location/pool/main/$owner/$repo
+  bash /usr/bin/gitaptly_scan_and_download.sh $repo_location/pool/main/$owner/$repo $owner $repo
 done < /etc/gitaptly.conf
 
 pushd $repo_location && dpkg-scanpackages --multiversion pool/ > dists/stable/main/binary-all/Packages && popd
