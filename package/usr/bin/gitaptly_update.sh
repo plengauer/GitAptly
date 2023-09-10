239b8b53fde0b54b9f50a4a9b7890ff548837dd2
plengauer <p.lengauer@gmail.com>
2023-09-10 15:16:47 +0000

    allow several version

--- package/usr/bin/gitaptly_update.sh
+++ package/usr/bin/gitaptly_update.sh
@@ -13,6 +13,6 @@ while read line; do
   bash /usr/bin/gitaptly_scan_and_download.sh $repo_location/pool/main $owner $repo
 done < /etc/gitaptly.conf
 
-pushd $repo_location && dpkg-scanpackages pool/ > dists/stable/main/binary-all/Packages && popd
+pushd $repo_location && dpkg-scanpackages --multiversion pool/ > dists/stable/main/binary-all/Packages && popd
 gzip -9 < $repo_location/dists/stable/main/binary-all/Packages > $repo_location/dists/stable/main/binary-all/Packages.gz
 bash /usr/bin/gitaptly_create_release.sh $repo_location/dists/stable > $repo_location/dists/stable/Release
