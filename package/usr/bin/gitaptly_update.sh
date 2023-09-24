216ddb70fc65296275899bd43530602617b97d45
plengauer <p.lengauer@gmail.com>
2023-09-24 19:42:24 +0000

    changed observability from legacy bash package to new shell package

--- package/usr/bin/gitaptly_update.sh
+++ package/usr/bin/gitaptly_update.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -e
 source /opt/gitaptly/env
-if [ -f /usr/bin/opentelemetry_bash.sh ]; then
+if [ -f /usr/bin/opentelemetry_shell.sh ]; then
   export OTEL_SERVICE_NAME=GitAptly
   export OTEL_EXPORTER_OTLP_TRACES_ENDPOINT="$OTLP_TRACES_ENDPOINT"
   export OTEL_EXPORTER_OTLP_TRACES_HEADERS=authorization=$(echo "$OTLP_TRACES_HEADER" | jq -Rr @uri)
-  source /usr/bin/opentelemetry_bash.sh
+  source /usr/bin/opentelemetry_shell.sh
   otel_instrument dpkg-scanpackages
 fi
 cd /var/lib/gitaptly
